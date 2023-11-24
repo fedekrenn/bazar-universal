c2f13850112ca0ec5411975ca32c35c141bcad59
Federico Krenn <fedekrenn@gmail.com>
2023-11-24 16:57:04 -0300

    Modify Cors

--- backend/main.js
+++ backend/main.js
@@ -8,14 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://bazar-universal-woad.vercel.app/",
-    ],
-  })
-);
+
+const corsOptions = {
+  origin: ['https://bazar-universal-woad.vercel.app', 'http://localhost:5173'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/items", itemRouter);
 
